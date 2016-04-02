38399d7fc35df3746f4545652580164616fdacdf
Fedor Troitsky <fedor.troitsky@gmail.com>
2016-04-03 02:15:31 +0300

    updated specs to use Chai BDD API

--- test/Player.spec.js
+++ test/Player.spec.js
@@ -1,6 +1,7 @@
 import {expect} from 'chai';
 import Map from '../src/Map';
 import Player from '../src/Player';
+import Tile from '../src/Tile';
 
 var map = new Map();
 map.setTile(0,0);
@@ -17,33 +18,33 @@ var player = new Player(map);
 
 describe("Player moving around map", () => {
     it("Player should be able to tell current position", () => {
-        expect(player.location.current.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
         expect(player.X).to.equal(0);
         expect(player.Y).to.equal(0);
     });
 
     it("Player should be able to tell correct exits", () => {
         expect(player.location.north).to.not.be.ok;
-        expect(player.location.south.constructor.name).to.equal('Tile');
+        expect(player.location.south).to.be.an.instanceof(Tile);
         expect(player.location.east).to.not.be.ok;
         expect(player.location.west).to.not.be.ok;
     });
 
     it("Player should be able to walk south", () => {
         player.walk('SOUTH');
-        expect(player.location.current.constructor.name).to.equal('Tile');
-        expect(player.location.north.constructor.name).to.equal('Tile');
-        expect(player.location.south.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
+        expect(player.location.north).to.be.an.instanceof(Tile);
+        expect(player.location.south).to.be.an.instanceof(Tile);
         expect(player.location.east).to.not.be.ok;
         expect(player.location.west).to.not.be.ok;
         expect(player.X).to.equal(0);
         expect(player.Y).to.equal(1);
 
         player.walk('SOUTH');
-        expect(player.location.current.constructor.name).to.equal('Tile');
-        expect(player.location.north.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
+        expect(player.location.north).to.be.an.instanceof(Tile);
         expect(player.location.south).to.not.be.ok;
-        expect(player.location.east.constructor.name).to.equal('Tile');
+        expect(player.location.east).to.be.an.instanceof(Tile);
         expect(player.location.west).to.not.be.ok;
         expect(player.X).to.equal(0);
         expect(player.Y).to.equal(2);
@@ -51,59 +52,59 @@ describe("Player moving around map", () => {
 
     it("Player should be able to walk east", () => {
         player.walk('EAST');
-        expect(player.location.current.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
         expect(player.location.north).to.not.be.ok;
         expect(player.location.south).to.not.be.ok;
-        expect(player.location.east.constructor.name).to.equal('Tile');
-        expect(player.location.west.constructor.name).to.equal('Tile');
+        expect(player.location.east).to.be.an.instanceof(Tile);
+        expect(player.location.west).to.be.an.instanceof(Tile);
         expect(player.X).to.equal(1);
         expect(player.Y).to.equal(2);
 
         player.walk('EAST');
-        expect(player.location.current.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
         expect(player.location.north).to.not.be.ok;
         expect(player.location.south).to.not.be.ok;
-        expect(player.location.east.constructor.name).to.equal('Tile');
-        expect(player.location.west.constructor.name).to.equal('Tile');
+        expect(player.location.east).to.be.an.instanceof(Tile);
+        expect(player.location.west).to.be.an.instanceof(Tile);
         expect(player.X).to.equal(2);
         expect(player.Y).to.equal(2);
 
         player.walk('EAST');
-        expect(player.location.current.constructor.name).to.equal('Tile');
-        expect(player.location.north.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
+        expect(player.location.north).to.be.an.instanceof(Tile);
         expect(player.location.south).to.not.be.ok;
         expect(player.location.east).to.not.be.ok;
-        expect(player.location.west.constructor.name).to.equal('Tile');
+        expect(player.location.west).to.be.an.instanceof(Tile);
         expect(player.X).to.equal(3);
         expect(player.Y).to.equal(2);
     });
 
     it("Player should be able to walk north", () => {
         player.walk('NORTH');
-        expect(player.location.current.constructor.name).to.equal('Tile');
-        expect(player.location.north.constructor.name).to.equal('Tile');
-        expect(player.location.south.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
+        expect(player.location.north).to.be.an.instanceof(Tile);
+        expect(player.location.south).to.be.an.instanceof(Tile);
         expect(player.location.east).to.not.be.ok;
         expect(player.location.west).to.not.be.ok;
         expect(player.X).to.equal(3);
         expect(player.Y).to.equal(1);
 
         player.walk('NORTH');
-        expect(player.location.current.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
         expect(player.location.north).to.not.be.ok;
-        expect(player.location.south.constructor.name).to.equal('Tile');
+        expect(player.location.south).to.be.an.instanceof(Tile);
         expect(player.location.east).to.not.be.ok;
-        expect(player.location.west.constructor.name).to.equal('Tile');
+        expect(player.location.west).to.be.an.instanceof(Tile);
         expect(player.X).to.equal(3);
         expect(player.Y).to.equal(0);
     });
 
     it("Player should be able to walk west", () => {
         player.walk('WEST');
-        expect(player.location.current.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
         expect(player.location.north).to.not.be.ok;
         expect(player.location.south).to.not.be.ok;
-        expect(player.location.east.constructor.name).to.equal('Tile');
+        expect(player.location.east).to.be.an.instanceof(Tile);
         expect(player.location.west).to.not.be.ok;
         expect(player.X).to.equal(2);
         expect(player.Y).to.equal(0);
@@ -111,14 +112,14 @@ describe("Player moving around map", () => {
 
     it("Player should not be able to walk on empty tile", () => {
         player.walk('WEST');
-        expect(player.location.current.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
         expect(player.X).to.equal(2);
         expect(player.Y).to.equal(0);
     });
 
     it("Player should not be able to walk off the map", () => {
         player.walk('NORTH');
-        expect(player.location.current.constructor.name).to.equal('Tile');
+        expect(player.location.current).to.be.an.instanceof(Tile);
         expect(player.X).to.equal(2);
         expect(player.Y).to.equal(0);
     });
